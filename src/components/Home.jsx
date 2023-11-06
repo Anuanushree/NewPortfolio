edc254b8c17f1c914a44f2ca3213a3e685b7422e
Anuanushree <anuabi2408@gmail.com>
2023-11-06 15:08:46 +0530

    completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -25,7 +25,7 @@ function Home() {
                     </Fade>
                     <Fade left>
                         <div className='d-flex justify-content-center'>
-                            <a href="https://drive.google.com/file/d/1l4dovHMEmTwMh4r7mg1zomeDNudOVhpP/view" target='_blank'>
+                            <a href="https://drive.google.com/file/d/1Red9nhaQDugW21bmaIx_a3pC58ygTY_S/view?usp=drivesdk" target='_blank'>
                                 <button className="theme-fill-btn "> View CV </button>
                             </a>
                         </div>
